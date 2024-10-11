f3e5c6d737778582d7ff092ebd667cf241469ad3
Darren Reid <github@reidmail.org>
2024-10-11 12:03:20 +1100

    Update example

--- Chinook/Program.cs
+++ Chinook/Program.cs
@@ -1,5 +1,7 @@
+using System.Globalization;
 using Chinook;
 using Chinook.ServiceInterface;
+using Microsoft.AspNetCore.Localization;
 using ServiceStack;
 
 ServiceStack.Licensing.RegisterLicense("OSS GPL-3.0 2024 https://github.com/NetCoreApps/Chinook I9j/48yaOXHt2eVVVZczPzbcGfC1k5njkND7V84Q/PLAOYHFx8KbmXHCluErNL+OeBaS+RnAZv3DEM6xKoG9gOB9ucRPCdpIoyxqhEaf40LonG+6GI7O+30t3WvVQJGI65Q64q+uVsRb025bysaEHfOf7Lf5YcKmwZvkao4nlfU=");
@@ -17,7 +19,29 @@ builder.Services.AddServiceStack(typeof(MyServices).Assembly, c => {
     });
 });
 
+var supportedCultures = new List<CultureInfo>
+{
+    new CultureInfo("en-GB"),
+    new CultureInfo("en-US"),
+    new CultureInfo("hu-HU")
+};
+
+var requestLocalizationOptions = new RequestLocalizationOptions
+{
+    DefaultRequestCulture = new RequestCulture("en-US"),
+    SupportedCultures = supportedCultures,
+    SupportedUICultures = supportedCultures
+};
+
+builder.Services.Configure<RequestLocalizationOptions>(options => {
+    options.DefaultRequestCulture = new RequestCulture(requestLocalizationOptions.DefaultRequestCulture.Culture.Name);
+    options.SupportedCultures = requestLocalizationOptions.SupportedCultures;
+    options.SupportedUICultures = requestLocalizationOptions.SupportedCultures;
+});
+
 var app = builder.Build();
+
+app.UseRequestLocalization(requestLocalizationOptions);
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
